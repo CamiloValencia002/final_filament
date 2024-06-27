baf120e42aac99a8b96da093d18a306d496883a0
INSTRUCTOR <INSTRUCTOR@PERAPRCSAFSD495.FormacionSena.RED>
2024-06-27 14:05:21 -0500

    Traducciones hechas nuevamente, ojala no se borren con los siguientes pushs

--- app/Models/Package.php
+++ app/Models/Package.php
@@ -15,18 +15,9 @@ class Package extends Model
     {
         return $this->belongsTo(Customer::class, 'id_customer');
     }
-<<<<<<< HEAD
 
     public function route()
     {
         return $this->belongsToMany(Route::class);
     }
-=======
-    
-    public function driver() {
-        return $this->belongsTo(Driver::class, 'id_driver');
-    }
- 
-
->>>>>>> aab255039f3cedb3a2feca9d7c6ed9ee14007069
 }
