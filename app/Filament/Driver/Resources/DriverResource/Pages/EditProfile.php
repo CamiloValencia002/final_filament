2d9e30cd825db981d2e75406f9bbddf3696caf9e
Alejandro  Martínez <tortugal2004@gmail.com>
2024-07-03 20:43:55 -0500

    Se arreglo el editar perfil

--- app/Filament/Driver/Resources/DriverResource/Pages/EditProfile.php
+++ app/Filament/Driver/Resources/DriverResource/Pages/EditProfile.php
@@ -1,50 +1,83 @@
 <?php
-
 namespace App\Filament\Driver\Resources\DriverResource\Pages;
 
-
-use Filament\Forms\Components\Button;
-use Illuminate\Support\Facades\Redirect;
+use Filament\Forms\Components\Section;
 use Filament\Forms\Components\FileUpload;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Toggle;
 use Filament\Forms\Form;
-use Filament\Notifications\Notification;
 use Filament\Pages\Auth\EditProfile as BaseEditProfile;
- 
+use Illuminate\Support\Facades\Storage;
+
 class EditProfile extends BaseEditProfile
 {
     public function form(Form $form): Form
     {
         return $form
             ->schema([
-                $this->getNameFormComponent(),
-                TextInput::make('last_name')
-                ->required()
-                ->maxLength(255),
-                $this->getEmailFormComponent(),
-                TextInput::make('telephone')
-                    ->tel()
-                    ->required()
-                    ->maxLength(255),
-                TextInput::make('adress')
-                    ->required()
-                    ->maxLength(255),
-                TextInput::make('document')
-                    ->required()
-                    ->maxLength(255),
-                Toggle::make('document_verify')
-                ->required()
-                ->disabled(),
-                FileUpload::make('photo_licence')
-                    ->image(),
-                FileUpload::make('image')
-                    ->image(),
+                Section::make('Información Personal')
+                    ->schema([
+                        $this->getNameFormComponent()
+                            ->label('Nombre')
+                            ->required()
+                            ->maxLength(255),
+                        TextInput::make('last_name')
+                            ->label('Apellido')
+                            ->required()
+                            ->maxLength(255),
+                        $this->getEmailFormComponent()
+                            ->label('Correo Electrónico'),
+                        TextInput::make('telephone')
+                            ->label('Teléfono')
+                            ->tel()
+                            ->required()
+                            ->maxLength(255),
+                        TextInput::make('adress')
+                            ->label('Dirección')
+                            ->required()
+                            ->maxLength(255),
+                    ])
+                    ->columns(2),
 
-                $this->getPasswordFormComponent(),
-                $this->getPasswordConfirmationFormComponent(),
+                Section::make('Documentación')
+                    ->schema([
+                        TextInput::make('document')
+                            ->label('Número de Documento')
+                            ->required()
+                            ->maxLength(255),
+                        Toggle::make('document_verify')
+                            ->label('Documento Verificado')
+                            ->required()
+                            ->disabled(),
+                        FileUpload::make('photo_licence')
+                            ->label('Foto de Licencia')
+                            ->image()
+                            ->directory('licencias'),
+                    ])
+                    ->columns(2),
+
+                Section::make('Foto de Perfil')
+                    ->schema([
+                        FileUpload::make('image')
+                        ->label('Foto de Perfil')
+                        ->image()
+                        ->directory('profile-photos')
+                        ->imageEditor()
+                        ->circleCropper()
+                        ->imageCropAspectRatio('1:1')
+                        ->imageResizeTargetWidth('300')
+                        ->imageResizeTargetHeight('300'),
+                    ]),
+
+                Section::make('Cambiar Contraseña')
+                    ->schema([
+                        $this->getPasswordFormComponent()
+                            ->label('Nueva Contraseña'),
+                        $this->getPasswordConfirmationFormComponent()
+                            ->label('Confirmar Nueva Contraseña'),
+                    ])
+                    ->columns(2),
             ]);
     }
-
 
 }
